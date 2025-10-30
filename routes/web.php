2ef4f373b992a6ee6412caf564f393aa42a131aa
Eduardo Cordova <eduardocordova1346@gmail.com>
2025-10-30 14:24:00 -0300

    Inicializado front-end com vue e o pacote de auth laravel/brezee

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,10 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
 
 Route::get('/', function () {
-    return view('welcome');
+    return redirect()->route('login');
 });
+
+require __DIR__.'/auth.php';
